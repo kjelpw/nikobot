0975918af246a718fca3c7927bc3395dc084ffcc
182421Kjel! <kjelpw@protonmail.com>
2022-03-13 20:53:22 -0600

    log page in flask for recent console outputs

--- app.py
+++ app.py
@@ -1,7 +1,68 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return '<p>Hello, World!</p>'
+
+@app.route('/log')
+def log():
+    print('accessing log')
+    fname = 'log.txt'
+    N = 4
+    bufsize = 8192
+    # calculating size of
+    # file in bytes
+    fsize = os.stat(fname).st_size
+
+    iter = 0
+     
+    # opening file using with() method
+    # so that file get closed
+    # after completing work
+    print('open file')
+    with open(fname) as f:
+        if bufsize > fsize:
+            
+            # adjusting buffer size
+            # according to size
+            # of file
+            bufsize = fsize-1
+            
+            # list to store
+            # last N lines
+            fetched_lines = []
+            
+            # while loop to
+            # fetch last N lines
+            print('looping')
+            while True:
+                iter += 1
+                
+                # moving cursor to
+                # the last Nth line
+                # of file
+                f.seek(fsize-bufsize * iter)
+                
+                # storing each line
+                # in list upto
+                # end of file
+                lines = f.read
+                print('for lines')
+                for line in f.readlines():
+                    fetched_lines.append('<p>' + str(line) + '</p>')
+                
+                # halting the program
+                # when size of list
+                # is equal or greater to
+                # the number of lines requested or
+                # when we reach end of file
+                print(len(fetched_lines))
+                if len(fetched_lines) >= N or f.tell() == 0:
+                    print('returning')
+                    return ''.join(fetched_lines[-N:])
+
+if __name__ == '__main__' :
+    app.run(host='0.0.0.0')
